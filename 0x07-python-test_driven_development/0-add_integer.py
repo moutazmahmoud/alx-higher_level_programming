052f751e9a193547029e107078db9b32c9c6685e
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-05-14 05:48:28 +0300

    Refactor: task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,9 +9,12 @@ def add_integer(a, b=98):
     >>> add_integer(3,6)
     9
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    else:
-        return a + b
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
