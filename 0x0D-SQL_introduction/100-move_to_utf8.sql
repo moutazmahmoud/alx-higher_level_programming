65aa898c04f847d6e2f789ea9a54bb3a36dc2f4c
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-05-22 05:04:05 +0300

    Refactor: Advanced tasks

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,6 @@
 -- Convert the database hbtn_0c_0 to UTF8 (utf8mb4) with collation utf8mb4_unicode_ci
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER DATABASE htbn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the table first_table to UTF8 (utf8mb4) with collation utf8mb4_unicode_ci
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert the field name in first_table to UTF8 (utf8mb4) with collation utf8mb4_unicode_ci
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
