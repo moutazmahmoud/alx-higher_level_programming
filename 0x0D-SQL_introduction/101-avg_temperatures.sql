293f2c23c7e962cb6d5b09f21e7dc909b76f99a6
moutazmahmoud <moutaz.prof.egy@gmail.com>
2024-05-22 05:10:46 +0300

    Refactor: Advanced tasks

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Calculates and displays the average temperature by city, ordered by temperature in descending order
-SELECT city, AVG(temperature) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- Displays the maximum temperature of each state, ordered by state name
 
-SELECT state, MAX(temperature) AS max_temp
-FROM temperatures
-GROUP BY state
-ORDER BY state;
+SELECT `state`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `state`
+ORDER BY `state`;
